c7198f272d31eb7ca6680758fe340b4951e15cd5
whizkelvin <kelvinmelchizedek@gmail.com>
2025-08-16 04:09:35 +0000

    home change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Clothes from "./pages/Clothes";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
